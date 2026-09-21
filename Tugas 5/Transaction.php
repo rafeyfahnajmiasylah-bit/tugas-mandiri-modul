f22c96862bbcd9cfe3792c753623225c92ad31f3
Rafeyfah Najmi Asylah <rafeyfahnajmiasylah@gmail.com>
2026-09-21 11:16:56 +0800

    Tambahkan proses deposit dan penarikan

--- Tugas 5/Transaction.php
+++ Tugas 5/Transaction.php
@@ -10,4 +10,31 @@ class Transaction
         private float $amount
     ) {
     }
+
+    public function process(float &$balance): bool
+    {
+        return match ($this->type) {
+            'deposit' => $this->processDeposit($balance),
+            'withdrawal' => $this->processWithdrawal($balance),
+            default => false,
+        };
+    }
+
+    private function processDeposit(float &$balance): bool
+    {
+        $balance += $this->amount;
+
+        return true;
+    }
+
+    private function processWithdrawal(float &$balance): bool
+    {
+        if ($this->amount > $balance) {
+            return false;
+        }
+
+        $balance -= $this->amount;
+
+        return true;
+    }
 }
